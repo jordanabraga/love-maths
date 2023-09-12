6708d0b5f5fcef8f9eb5d48ccc7b37f412961883
jordanabraga <>
2023-09-12 23:58:35 +0000

    JS variables doc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("slay")
+// doc variables
+
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let displayContainer = document.getElementById("quiz-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+
+let scoreContainer = document.querySelector(".score-container");
+let startScreen = document.querySelector(".start-screen");
+let timeLeft = document.querySelector(".time-left");
